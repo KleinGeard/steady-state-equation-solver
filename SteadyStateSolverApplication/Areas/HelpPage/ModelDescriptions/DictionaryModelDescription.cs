85c53fd00021d43aa97d788a7c42eb01f4bc5f95
Klein Geard <kleinmarkgeard@gmail.com>
2018-06-26 16:48:07 +1000

    No longer solves pi_k equiv when pi_k is not within the equation +Properly removed unused web applications

--- SteadyStateSolverApplication/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SteadyStateSolverApplication.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- SteadyStateSolverApplication/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace SteadyStateSolverApplication.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
